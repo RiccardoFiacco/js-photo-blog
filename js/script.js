a960ead04d0ea3bb255d352b5fec951b28487597
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-06 17:11:44 +0100

    add the function to create the cards

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-function axiosCall( n){
+function axiosCall(callback, n){
     const staticUrl = "https://jsonplaceholder.typicode.com/";
     let url_body = 'photos'
     let  endpoint = staticUrl+ url_body;
@@ -10,21 +10,28 @@ function axiosCall( n){
 			_limit: n,
 		},
 	})
-    .then((res)=>{
-        console.log(res.data)
+    .then((res) => {
+        let array = res.data
+        callback(array)
     })
     .catch((err) => console.log(err));
-}
-let literal = `<div class="col-sm-12 col-md-6 col-lg-4">
-                    <div class="card">
-                        <img src="./img/pin.svg" class="position-absolute left-50 transform-trans-50 top-10px" alt="...">
-                        <img src="..." class="card-img-top p-15 img-card" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>`
+};
 
+function createCards(array){
+    array.forEach(el => {
+        console.log(el.url)
+        cards.innerHTML +=
+        `<div class="col-sm-12 col-md-6 col-lg-4">
+                <div class="card">
+                    <img src="./img/pin.svg" class="position-absolute left-50 transform-trans-50 top-10px" alt="...">
+                    <img src="${el.url}" class="card-img-top p-15 img-card" alt="...">
+                <div class="card-body">
+                    <p class="card-text">${el.title}</p>
+                </div>
+            </div>
+        </div>`
+    });
+}
 //chiamo una funzione passandogli una callaback per creare le cards con la risposta della chiamata axios 
 let cardNum = 6;
-axiosCall( cardNum);
+axiosCall(createCards, cardNum);
