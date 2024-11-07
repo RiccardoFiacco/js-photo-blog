ea75d81ca0a12a3b73fdbdf46c708331193357aa
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-07 15:24:29 +0100

    add see Overlay and close overlay

--- js/script.js
+++ js/script.js
@@ -19,13 +19,11 @@ function axiosCall(callback, n){
 
 function createCards(array){
     array.forEach(el => {
-        // let app = [el.id, el.url]
-        // console.log(app)
         cards.innerHTML +=
         `<div class="col-sm-12 col-md-6 col-lg-4">
                 <div class="card" id="${el.id}">
                     <img src="./img/pin.svg" class="position-absolute left-50 transform-trans-50 top-10px" alt="...">
-                    <img src="${el.url}" onclick="seeOverlay(${el.id},'${el.url}')" class="card-img-top p-15 img-card" id="photo" alt="...">
+                    <img src="${el.url}" onclick="seeOverlay('${el.url}')" class="card-img-top p-15 img-card" id="photo" alt="...">
                 <div class="card-body">
                     <p class="card-text">${el.title}</p>
                 </div>
@@ -34,19 +32,18 @@ function createCards(array){
     });
 }
 
-function seeOverlay(id, url){
-    console.log(id, url)
-    const app = document.getElementById(id);
-    app.addEventListener("click", (event)=>{
-        event.preventDefault();    
-        overlay.style.display = "block";
-        app.src = url; 
-    })
 
-    closeOverlay.addEventListener("click", (event)=>{
-        event.preventDefault();
+
+function seeOverlay(url){
+
+    const app = document.getElementById("img-overlay");
+    overlay.style.display = "block";
+    app.src = url; 
+
+    overlay.addEventListener("click", (event)=>{
         overlay.style.display = "none";
     })
+
 }
 
 //chiamo una funzione passandogli una callaback per creare le cards con la risposta della chiamata axios 
